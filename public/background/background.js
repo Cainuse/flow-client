9d85d3dc7754d6c0c61d98b2be78c31d90a071c8
Zhen Peng Zhu <zhenpengzhu@Zhens-MacBook-Pro.local>
2019-03-26 20:24:05 -0700

    Added scrolling, display changing, webpage navigation features

--- public/background/background.js
+++ public/background/background.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-///<reference types="chrome"/>
-/// <reference types="uuid"/>
+//<reference types="chrome"/>
+// <reference types="uuid"/>
 
 function helperNumberOfTabs() {
   chrome.tabs.query({ windowType: "normal" }, function(tabs) {
@@ -49,22 +49,40 @@ function commandDeleteTab(parsedMessage) {
   });
 }
 
-function createWebSocketConnection() {
-  let websocket;
-  let isCommandSuccessful;
+async function createWebSocketConnection() {
+  let websocket, isCommandSuccessful;
+  let userEmail;
+  let userId;
+  await chrome.identity.getProfileUserInfo(user => {
+    userEmail = user.email;
+    userId = user.id;
+  });
+  console.log(userEmail);
+  // try{
+
+  //   console.log(userInfo);
+  // }catch{
+  //   alert("Sorry, you may not be logged into your browser, as such we cannot create connection to our server!");
+  //   return;
+  // }
+
   if ("WebSocket" in window) {
-    websocket = new WebSocket("ws://localhost:9090/ws");
+    websocket = new WebSocket("ws://localhost:8080/ws");
 
     websocket.onopen = function() {
-      websocket.send("client reached out");
+      console.log(`{"email":${userEmail}, "action":"Sign In"}`);
+      websocket.send(
+        `{"email":"ABwppHHUAGaEOI2o_jIDCkQpD5re88q4jCSvDe80qoCH1ysCz2eQ8UJk-pY8uP2ccdsqdZud1_NhrA", "action":"Sign In"}`
+      );
+
+      console.log("client did reach out");
     };
 
-    // websocket.onmessage = function(e) {
-    //   console.log(e.data);
-    // };
-
     websocket.onmessage = function(event) {
+      console.log("Entering on message listener");
       if (event.data != null) {
+        console.log("Data is not null");
+        console.log(JSON.parse(event.data));
         isCommandSuccessful = messageHandler(JSON.parse(event.data));
         websocket.send(isCommandSuccessful);
       }
@@ -83,14 +101,171 @@ function endSession(websocket) {
 
 // Message marshalling
 function messageHandler(parsedMessage) {
-  switch (parsedMessage.commandType) {
-    case 1:
-      return commandCreateTab(parsedMessage);
-    case 2:
-      return commandDeleteTab(parsedMessage);
-    case 3:
-      return switchTab(parsedMessage);
-    default:
-      return false;
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true
+    },
+    tabArray => {
+      let extraParam = {
+        tabId: tabArray[0].id,
+        winId: tabArray[0].windowId
+      };
+      switch (parsedMessage.command) {
+        case "Scroll":
+          return scrollPage(parsedMessage.param, extraParam);
+        case "Change Scrolling Speed":
+          return changeScrollingSpeed(parsedMessage.param, extraParam);
+        case "Change Browser Display":
+          return changeStateOfBrowser(parsedMessage.param, extraParam);
+        case "Navigate Webpage":
+          return goToWebPage(parsedMessage.param, extraParam);
+        default:
+          return false;
+      }
+    }
+  );
+}
+
+/**
+ * Changes the display state of browser
+ * @param {*} win
+ */
+
+async function changeStateOfBrowser(param, extraParam) {
+  console.log("Activated!");
+  let availableStates = ["normal", "minimized", "maximized", "fullscreen"];
+
+  locParam = param.fields.DisplayMode.Kind.StringValue;
+  if (availableStates.includes(locParam)) {
+    chrome.tabs.query(
+      {
+        currentWindow: true,
+        active: true
+      },
+      tabArray => {
+        chrome.windows.update(tabArray[0].windowId, {
+          state: locParam
+        });
+        console.log(`Updating browser size to ${locParam}`);
+      }
+    );
   }
 }
+
+let scrollParam = {
+  speed: 5,
+  direction: null
+};
+
+let scroll = null;
+/**
+ * Scrolls down on the highlighted tab
+ * @param {*} win
+ */
+function scrollPage(param, extraParam) {
+  let x = 0;
+  let y = 0;
+  locParam = param.fields.Direction.Kind.StringValue;
+  scrollParam.direction = locParam;
+  switch (scrollParam.direction) {
+    case "up":
+      x = 0;
+      y = -scrollParam.speed;
+      break;
+    case "down":
+      x = 0;
+      y = scrollParam.speed;
+      break;
+    case "right":
+      x = scrollParam.speed;
+      y = 0;
+      break;
+    case "left":
+      x = -scrollParam.speed;
+      y = 0;
+      break;
+    default:
+      x = 0;
+      y = 0;
+  }
+  scrollPageHelper(x, y, extraParam);
+}
+
+function changeScrollingSpeed(param, extraParam) {
+  switch (param.fields.ScrollingSpeed.Kind.StringValue) {
+    case "slow":
+      scrollParam.speed = 5;
+      break;
+    case "middle":
+      scrollParam.speed = 10;
+      break;
+    case "fast":
+      scrollParam.speed = 15;
+      break;
+    case "lightning":
+      scrollParam.speed = 30;
+      break;
+    default:
+      scrollParam.speed = 0;
+      break;
+  }
+  scrollPageHelper(x, y, extraParam);
+}
+
+function scrollPageHelper(x, y, extraParam) {
+  if (scroll != null) {
+    clearInterval(scroll);
+  }
+  if (scrollParam.direction != "stop") {
+    scroll = setInterval(() => {
+      chrome.tabs.executeScript(extraParam.tabId, {
+        code: `window.scrollBy({top: ${y}, left: ${x}, behavior: 'smooth'})`
+      });
+    }, 100);
+  }
+}
+function goForwardOrBackward(param) {
+  if (param == "forward") {
+    chrome.tabs.goForward(currTab.id);
+  } else if (param == "backward") {
+    chrome.tabs.goBack(currTab.id);
+  }
+}
+
+function refresh(tab) {
+  chrome.tabs.reload();
+}
+
+// NOT YET SUPPORTED
+function captureScreen(tab) {
+  win = chrome.windows.getCurrent;
+  chrome.tabs.captureVisibleTab(dataUrl => {
+    console.log(dataUrl);
+  });
+}
+
+function goToWebPage(param, extraParam) {
+  let urlParam = param.fields.url.Kind.StringValue;
+  if (
+    !urlParam.includes(".com") &&
+    !urlParam.includes(".ca") &&
+    !urlParam.includes(".net") &&
+    !urlParam.includes(".org") &&
+    !urlParam.includes(".cn") &&
+    !urlParam.includes(".gov")
+  ) {
+    urlParam += ".com";
+  }
+  chrome.tabs.update(extraParam.tabId, {
+    url: "https://www." + urlParam.replace("www.", "")
+  });
+}
+// let currTab = currTab = chrome.tabs.getCurrent(tab => {
+//   return tab;
+// });
+
+let currWin = null;
+function start(win) {}
+
+chrome.browserAction.onClicked.addListener(start);
