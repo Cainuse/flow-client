91f9b7fceface6f4abcb46a2de85e773dd529f23
Zhen Peng Zhu <zhenpengzhu@Zhens-MacBook-Pro.local>
2019-03-27 23:17:54 -0700

    Added support for James' original tabular functions

--- public/background/background.js
+++ public/background/background.js
@@ -2,51 +2,81 @@
 //<reference types="chrome"/>
 // <reference types="uuid"/>
 
-function helperNumberOfTabs() {
-  chrome.tabs.query({ windowType: "normal" }, function (tabs) {
-    console.log(
-      "Number of open tabs in all normal browser windows:",
-      tabs.length
-    );
-  });
-}
 
-function switchTab(parsedMessage) {
-  chrome.windows.getCurrent({ populate: true }, function (windowsArray) {
-    const currentWindowId = windowsArray.id;
-    if (numberString < windowsArray.tabs.length) {
+function switchTab(param, extraParam) {
+  //Left Right
+  if (param.fields.Direction.Kind.StringValue != "") {
+    locParam = param.fields.Direction.Kind.StringValue;
+    chrome.tabs.get(extraParam.tabId, (tab) => {
+      tabInd = tab.index;
+      offset = 1;
+      if (locParam == "left") {
+        offset = -1;
+      }
+      if (tabInd)
+        chrome.tabs.query(
+          { index: (tabInd + offset), windowId: extraParam.winId },
+          (tabs) => {
+            if (tabs.length > 0) {
+              chrome.tabs.update(tabs[0].id, { active: true });
+            }
+          }
+        );
+    })
+  }
+  //Leftmost Rightmost
+  else if (param.fields.TabDirection.Kind.StringValue) {
+    locParam = param.fields.TabDirection.Kind.StringValue;
+    locIndex = 1;
+    chrome.windows.get(extraParam.winId, { populate: true }, (win) => {
+      if (locParam == "rightmost") {
+        locIndex = win.tabs.length
+      }
       chrome.tabs.query(
-        { index: parseInt(numberString), windowId: currentWindowId },
-        function (tabs) {
-          chrome.tabs.update(tabs[0].id, { active: true });
+        { index: (locIndex - 1), windowId: extraParam.winId },
+        (tabs) => {
+          if (tabs.length > 0) {
+            console.log(tabs[0].id)
+            chrome.tabs.update(tabs[0].id, { active: true });
+          }
         }
       );
-    } else {
-      new Error("Switch Tab function Error");
-    }
-  });
-}
-
-function commandCreateTab(parsedMessage) {
-  chrome.windows.getCurrent({ populate: true }, function (windowsArray) {
-    const currentWindowId = windowsArray.id;
-    chrome.tabs.create(
-      { windowId: currentWindowId, active: true, index: 0 },
-      () => {
-        return true;
+    })
+  }
+  //Index based
+  else if (param.fields.number.Kind.NumberValue) {
+    locParam = param.fields.number.Kind.NumberValue;
+    chrome.tabs.query(
+      { index: (locParam - 1), windowId: extraParam.winId },
+      (tabs) => {
+        if (tabs.length > 0) {
+          console.log(tabs[0].id)
+          chrome.tabs.update(tabs[0].id, { active: true });
+        }
       }
     );
-  });
+  }
+
+
 }
 
-function commandDeleteTab(parsedMessage) {
-  chrome.windows.getCurrent({ populate: true }, function (windowsArray) {
-    if (numberString < windowsArray.tabs.length) {
-      chrome.tabs.discard(numberString, () => {
-        // successful callback
-      });
-    }
-  });
+function createTab(param, extraParam) {
+  chrome.tabs.create(
+    { windowId: extraParam.winId, active: true }
+  )
+}
+
+function deleteTab(param, extraParam) {
+  locStr = param.fields.number.Kind.StringValue;
+  locInd = param.fields.number.Kind.NumberValue;
+  if (locStr == "") {
+    chrome.tabs.remove(extraParam.tabId);
+  } else if (locInd != null) {
+    chrome.tabs.query({ index: (locInd - 1) }, (tabs) => {
+      chrome.tabs.remove(tabs[0].id);
+    })
+  }
+
 }
 
 async function createWebSocketConnection() {
@@ -128,6 +158,12 @@ function messageHandler(parsedMessage) {
           return goToWebPage(parsedMessage.param, extraParam);
         case "PageAction":
           return backwardForwardRefreshPage(parsedMessage.param, extraParam)
+        case "Select Tab":
+          return switchTab(parsedMessage.param, extraParam)
+        case "Delete Tab":
+          return deleteTab(parsedMessage.param, extraParam)
+        case "Create Tab":
+          return createTab(parsedMessage.param, extraParam)
         default:
           return false;
       }
