2e3c884945046b0c65faac55d0593f27c6bdae8b
James Kan <james0077721@hotmail.com>
2019-03-28 18:37:19 -0700

    Cleaning up.

--- public/background/background.js
+++ public/background/background.js
@@ -1,15 +1,12 @@
 /* eslint-disable no-undef */
 //<reference types="chrome"/>
 // <reference types="uuid"/>
-async function main(){
+async function main() {
   await createWebSocketConnection();
 }
 main();
 
 //For testing only
-function start(win) {
-}
+function start(win) {}
 
 chrome.browserAction.onClicked.addListener(start);
-
-
